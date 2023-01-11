0d979d53df7f89803b6c53353fa3513219141aa8
Victor Santos Silva <victor.stsilva93@gmail.com>
2023-01-10 21:28:57 -0300

    trabalhando com os dados no localStorage

--- main.js
+++ main.js
@@ -1,38 +1,42 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
-const itens = [];
+const itens = JSON.parse(localStorage.getItem('itens')) || []; //JSON.parse - transforma a string JSON em dados para o javascript (no caso volta a ser uma array de objetos).
+
+itens.forEach((element) => {
+    criaElemento(element);
+}) //cria o elemento html independente do 'submit', buscando ele no localStorage (var 'itens'), mantendo o elemento sempre que a pagina atualizar.
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
+    event.preventDefault(); //utilizado para cancelar o evento (no caso, evitar que a pagina atualize).
 
     const nome = event.target.elements['nome'];
-    const quantidade = event.target.elements['quantidade'];
+    const quantidade = event.target.elements['quantidade']; // armazena em variáveis os valores digitados.
 
-    criaElemento(nome.value, quantidade.value);
+    const itemAtual = { //cria um objeto com as variáveis dos valores digitados.
+        'nome': nome.value,
+        'quantidade': quantidade.value
+    }
+
+    itens.push(itemAtual); //adiciona o objeto à array com os itens do localStorage.
+
+    criaElemento(itemAtual); //cria o elemento html com o objeto criado.
 
     nome.value = '';
-    quantidade.value = '';
+    quantidade.value = ''; //limpa os valores digitados no campo de imput.
 })
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
 
     const novoItem = document.createElement('li');
     novoItem.classList.add('item');
 
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
+    numeroItem.innerHTML = item.quantidade;
 
-    novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
+    novoItem.appendChild(numeroItem); //utiliza-se o appendChild para incluir a tag, pois tag é considerada um objeto.
+    novoItem.innerHTML += item.nome;
 
     lista.appendChild(novoItem);
 
-    const itemAtual = {
-        'nome': nome,
-        'quantidade': quantidade
-    }
-
-    itens.push(itemAtual);
-
-    localStorage.setItem('item', JSON.stringify(itens));
+    localStorage.setItem('itens', JSON.stringify(itens)); //JSON.stringfy - transforma valores e objetos em uma string JSON (no caso para armazenar no localStorage, que só aceita strings).
 }
